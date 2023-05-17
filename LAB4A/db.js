886e03f0e5815c60526b9bbf162fd4f3e87f4133
WojciechWnukk <131540870+WojciechWnukk@users.noreply.github.com>
2023-05-17 17:53:22 +0200

    Update db.js

--- LAB4A/db.js
+++ LAB4A/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 //mongoose.connect("mongodb://127.0.0.1:27017/StudentDB", { useNewUrlParser: true })
-mongoose.connect("mongodb+srv://wojti767:Wojtek123@veb.1ouiowz.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true })
+mongoose.connect("mongodb+srv://wojti767:HASLOTRZEBAWPISACNOWE@veb.1ouiowz.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true })
     .then((result) => {
     console.log("Połączono z bazą")
     }).catch((err) => {
